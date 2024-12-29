f42fd2e3792cf3a46d20277ce00ce37fc4e75e62
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2024-12-29 20:46:22 +0000

    Basic Design

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom'; 
+import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown'; // Import a Markdown rendering library
-
+import './Post.css';
 
 function Post() {
-  const [post, setPost] = useState(null);
-  const { id } = useParams(); 
+    const [post, setPost] = useState(null);
+    const { id } = useParams();
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const res = await axios.get(`https://symmetrical-invention-gj6gq6q6j44h9w9x-5000.app.github.dev/api/posts/${id}`);
-        setPost(res.data);
-      } catch (error) {
-        console.error('Error fetching post:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const res = await axios.get(`https://symmetrical-invention-gj6gq6q6j44h9w9x-5000.app.github.dev/api/posts/${id}`);
+                setPost(res.data);
+            } catch (error) {
+                console.error('Error fetching post:', error);
+            }
+        };
 
-    fetchPost();
-  }, [id]); 
+        fetchPost();
+    }, [id]);
 
-  if (!post) {
-    return <div>Loading...</div>;
-  }
+    if (!post) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>By {post.author}</p>
-      {post.imageUrl && <img src={post.imageUrl} alt={post.title} />} 
-      <ReactMarkdown children={post.content} /> 
-      <h2>Actual content</h2>
-      <p>{post.content}</p>
-    </div> 
-  );
+    return (
+
+        <div className="post-container">
+            <h2>{post.title}</h2>
+            <p>By {post.author}</p>
+            <div className="post-image">
+                {post.imageUrl && <img src={post.imageUrl} alt={post.title} />}
+            </div>
+            <div className="post-content">
+                <ReactMarkdown children={post.content} />
+            </div>
+        </div>
+    );
 }
 
 export default Post;
